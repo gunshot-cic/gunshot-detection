d4e6a9f7f286b9dd67f356fba7c4b736180bd26c
krishnasameeth <skris114@asu.edu>
2022-04-14 14:06:08 -0700

    sameet: changes

--- server/app/controllers/incidents.controller.js
+++ server/app/controllers/incidents.controller.js
@@ -26,3 +26,35 @@ exports.getIncidents = (req, res) => {
     }
   });
 };
+
+exports.uploadIncident = (req, res) => {
+  const device_id = req.body.device_id;
+  const s3url = req.body.s3_url;
+  const notification = req.body.notification;
+
+  AWS.config.update(config.aws_remote_config);
+
+  const docClient = new AWS.DynamoDB.DocumentClient();
+
+  const params = {
+    TableName: config.aws_table_name,
+    Item: {
+      device_id: device_id,
+      s3_url: s3url,
+      notification: notification,
+    },
+  };
+
+  docClient.put(params, function (err, data) {
+    if (err) {
+      console.log(err);
+      res.json({
+        success: false,
+      });
+    } else {
+      res.json({
+        success: true,
+      });
+    }
+  });
+};
