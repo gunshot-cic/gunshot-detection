3bdabab9b3e09cf2c17bc2bb9250e216c73bb309
krishnasameeth <skris114@asu.edu>
2022-04-14 19:52:35 -0700

    sameet:changes

--- http.js
+++ http.js
@@ -1,24 +1,28 @@
 const cors = require("cors");
 var querystring = require("querystring");
-var path = require('path');
-var express = require('express')
+var path = require("path");
+var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 app.use(methodOverride());
 app.use(bodyParser.json()); // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 app.get("/", (req, res) => {
-  res.redirect("index.html")
+  res.redirect("index.html");
 });
 
 require("./server/app/routes/incidents.routes")(app);
 require("./server/app/routes/devices.routes")(app);
 
 // Server client (static files)
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 // DO NOT DO app.listen() unless we're testing this directly
-if (require.main === module) { app.listen(8080); }
+if (require.main === module) {
+  app.listen(8080);
+}
 // Instead do export the app:
-else{ module.exports = app; }
+else {
+  module.exports = app;
+}
--- https.js
+++ https.js
@@ -1,13 +1,18 @@
-var homeDir = require('path').join(require('os').homedir());
-require('greenlock-express').create({
-  version: 'draft-11'
-, server: 'https://acme-v02.api.letsencrypt.org/directory'
-//, server: 'https://acme-staging-v02.api.letsencrypt.org/directory'  // staging
-, email: 'tejaswi@asu.edu'                                     // CHANGE THIS
-, agreeTos: true
-, approveDomains: [ 'costarica.acousticecologylab.org', 'www.costarica.acousticecologylab.org' ]              // CHANGE THIS
-, store: require('greenlock-store-fs')
-, configDir: homeDir
-, app: require('./http.js')
-//, communityMember: true
-}).listen(8080, 8443);
+var homeDir = require("path").join(require("os").homedir());
+require("greenlock-express")
+  .create({
+    version: "draft-11",
+    server: "https://acme-v02.api.letsencrypt.org/directory",
+    //, server: 'https://acme-staging-v02.api.letsencrypt.org/directory'  // staging
+    email: "asucicoffice@gmail.com", // CHANGE THIS
+    agreeTos: true,
+    approveDomains: [
+      "asucic-gunshotdetection.com",
+      "www.asucic-gunshotdetection.com",
+    ], // CHANGE THIS
+    store: require("greenlock-store-fs"),
+    configDir: homeDir,
+    app: require("./http.js"),
+    //, communityMember: true
+  })
+  .listen(8080, 8443);
