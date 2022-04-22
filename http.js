3fe37f41525a968d61066c43a68af22bf3e0907e
krishnasameeth <skris114@asu.edu>
2022-04-22 11:57:57 -0700

    changes

--- http.js
+++ http.js
@@ -1,28 +1,67 @@
 const cors = require("cors");
+var url = require("url");
+var passport = require("passport");
+var fs = require("fs");
+
 var querystring = require("querystring");
 var path = require("path");
 var express = require("express");
+
+var dbURL = "mongodb://127.0.0.1:27017/gunshot_db";
+var db = require("mongoskin").db(dbURL);
+var mongoose = require("mongoose");
+mongoose.connect(dbURL);
+
 var app = express();
+
+var secret = "test" + new Date().getTime().toString();
+var session = require("express-session");
+app.use(require("cookie-parser")(secret));
+var MongoStore = require("connect-mongo")(session);
+app.use(
+  session({
+    store: new MongoStore({
+      url: dbURL,
+      secret: secret,
+    }),
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+var flash = require("express-flash");
+app.use(flash());
+
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 app.use(methodOverride());
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("<h1>GUNSHOT SITE UNDER CONSTRUCTION!</h1>");
-});
+require("./passport/config/passport")(passport); // pass passport for configuration
+require("./passport/routes.js")(app, passport); // load our routes and pass in our app and fully configured passport
 
 require("./server/app/routes/incidents.routes")(app);
 require("./server/app/routes/devices.routes")(app);
 
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
+
 // Server client (static files)
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "server/app/public")));
 // DO NOT DO app.listen() unless we're testing this directly
 if (require.main === module) {
+  console.log("RUNNING");
   app.listen(8080);
 }
 // Instead do export the app:
 else {
   module.exports = app;
+}
+
+// route middleware to ensure user is logged in
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+
+  res.send("noauth");
 }
