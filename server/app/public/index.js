536c144c669c6a2fbe252f1eec110346a95acaa5
krishnasameeth <skris114@asu.edu>
2022-04-29 18:09:08 -0700

    changes

--- server/app/public/index.js
+++ server/app/public/index.js
@@ -17,7 +17,7 @@ function subscribe() {
     number: number,
   };
 
-  fetch("http://localhost:8080/api/sns/subscribe", {
+  fetch("https://asucic-gunshotdetection/api/sns/subscribe", {
     method: "POST",
     body: JSON.stringify(data),
     headers: {
@@ -38,7 +38,7 @@ function unsubscribe() {
     number: number,
   };
 
-  fetch("http://localhost:8080/api/sns/unsubscribe", {
+  fetch("https://asucic-gunshotdetection/api/sns/unsubscribe", {
     method: "POST",
     body: JSON.stringify(data),
     headers: {
