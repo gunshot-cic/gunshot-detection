561cd78ef1449b0c61acc8e7840ab79ea11880b2
krishnasameeth <skris114@asu.edu>
2022-05-04 12:51:48 -0700

    changes

--- server/app/controllers/incidents.controller.js
+++ server/app/controllers/incidents.controller.js
@@ -85,15 +85,16 @@ exports.uploadIncident = (req, res) => {
       res.json({
         success: true,
       });
-      const lambda = new AWS.Lambda();
-      params = {
-        FunctionName: "publishMessages" /* required */,
-        Payload: JSON.stringify({ lat: 28.7041, long: 77.1025 }),
-      };
-      lambda.invoke(params, function (err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else console.log("INVOKED LAMBDA ", data); // successful response
-      });
+      // this is for testing purposes only
+      // const lambda = new AWS.Lambda();
+      // params = {
+      //   FunctionName: "publishMessages" /* required */,
+      //   Payload: JSON.stringify({ lat: 28.7041, long: 77.1025 }),
+      // };
+      // lambda.invoke(params, function (err, data) {
+      //   if (err) console.log(err, err.stack); // an error occurred
+      //   else console.log("INVOKED LAMBDA ", data); // successful response
+      // });
     }
   });
 };
